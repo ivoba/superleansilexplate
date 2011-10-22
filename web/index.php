34fb8c27a384433e43cf346fc70a70e63425fa79
ivo <ivo.bathke@gmail.com>
2011-10-22 19:55:04 +0200

    twig

--- web/index.php
+++ web/index.php
@@ -5,10 +5,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Silex\Provider\HttpCacheServiceProvider;
-use Silex\Provider\SessionServiceProvider;
-use Silex\Provider\TranslationServiceProvider;
-use Silex\Provider\TwigServiceProvider;
 
 $app = new Silex\Application();
 
@@ -32,18 +28,15 @@ else{
     $app['environment'] = 'prod';
 }
 
-$app->register(new HttpCacheServiceProvider());
-$app->register(new SessionServiceProvider());
-$app->register(new TranslationServiceProvider(), array(
+$app->register(new Silex\Provider\HttpCacheServiceProvider());
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
     'locale_fallback' => $app['locale'],
 ));
-$app['translator.messages'] = require_once __DIR__ . '/../resources/locales/translations.php';
 
-$app->register(new TwigServiceProvider(), array(
-    'twig.options'  => array('cache' => false, 'strict_variables' => true),
-    'twig.path'     => array(
-        __DIR__ . '/../views'
-    )
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path'       => __DIR__.'/../views',
+    'twig.class_path' => __DIR__.'/../vendor/twig/lib',
 ));
 
 //set layout template
