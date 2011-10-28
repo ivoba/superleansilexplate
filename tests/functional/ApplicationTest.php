92322e50cc136eef8a250a3ee38d8b8923c110db
ivo <ivo.bathke@gmail.com>
2011-10-28 10:52:10 +0200

    some translation test, not complete yet

--- tests/functional/ApplicationTest.php
+++ tests/functional/ApplicationTest.php
@@ -28,21 +28,35 @@ class ApplicationTest extends WebTestCase
     public function testIndex(){
     	$client = $this->createClient();
 	$crawler = $client->request('GET', '');
+        $crawler = $client->followRedirect();
 	$this->assertTrue($client->getResponse()->isOk());
-    }
-    public function testTranslate(){
-        $client = $this->createClient();
-	$crawler = $client->request('GET', '');
-	$this->assertTrue($client->getResponse()->isOk());
-	//TODO fetch values from translations
+        //TODO fetch values from translations
 	$this->assertTrue($crawler->filter('html:contains("Homepage")')->count() > 0);
     }
     public function testTranslateDe(){
         $this->app['locale'] = 'de';
 	$client = $this->createClient();
 	$crawler = $client->request('GET', '');
+        $crawler = $client->followRedirect();
 	$this->assertTrue($client->getResponse()->isOk());
 	//TODO fetch values from translations
 	$this->assertTrue($crawler->filter('html:contains("Startseite")')->count() > 0);
+    }
+    public function testLanguageRoutes(){
+        $client = $this->createClient();
+	$client->followRedirects();
+        $crawler = $client->request('GET', '/de');
+        $this->assertTrue($client->getResponse()->isOk());
+        $this->assertTrue($crawler->filter('html:contains("Kopf")')->count() > 0);
+	$crawler = $client->request('GET', '/de/index');
+        $this->assertTrue($client->getResponse()->isOk());
+        $this->assertTrue($crawler->filter('html:contains("Kopf")')->count() > 0);
+        $crawler = $client->request('GET', '/fr');
+        $this->assertTrue($client->getResponse()->isOk());
+        //TODO not working, still returns de should return fr
+        #var_dump($crawler->text());
+        #$this->assertTrue($crawler->filter('html:contains("Pied")')->count() > 0);
+	$crawler = $client->request('GET', '/aa');
+        $this->assertTrue($client->getResponse()->isNotFound());
     }
 }
