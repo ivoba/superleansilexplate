c012532200bdbb4638b8f672e9d62c2c3cf9e721
ivo <ivo.bathke@gmail.com>
2011-11-02 12:53:39 +0100

    getskeleton.com integration

--- resources/locales/translations.php
+++ resources/locales/translations.php
@@ -4,19 +4,25 @@ return array(
     'en' => array(
         'homepage' => 'Homepage',
         'head' => 'Head',
-        'content' => 'Content',
+        'content' => array('about' => 'About',
+                           'features' => 'Features',
+                           'contribute' => 'Contribute'),
         'foot' => 'Foot'
     ),
     'de' => array(
         'homepage' => 'Startseite',
         'head' => 'Kopf',
-        'content' => 'Inhalt',
+        'content' => array('about' => 'Über',
+                           'features' => 'Eigenschaften',
+                           'contribute' => 'Mitmachen'),
         'foot' => 'Fuß'
     ),
     'fr' => array(
         'homepage' => 'Acculeil',
         'head' => 'Tête',
-        'content' => 'Contenu',
+        'content' => array('about' => 'A propos',
+                           'features' => 'Propriétés',
+                           'contribute' => 'Contribuer'),
         'foot' => 'Pied'
     ),
 );
