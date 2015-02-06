9674e28d89563f507878f24b676c1ed6660da723
Ivo Bathke <ivo.bathke@gmail.com>
2015-02-06 17:26:10 +0100

    travis adjustments

--- src/app.php
+++ src/app.php
@@ -5,11 +5,11 @@ use Silex\Provider\HttpCacheServiceProvider;
 $app = new Silex\Application();
 
 //you might want to use https://github.com/ivoba/dotenv-service-provider here
-$app['environment'] = getenv('SILEX_ENV') ? getenv('SILEX_ENV') : 'dev';
+$app['environment'] = getenv('SILEX_ENV') ? getenv('SILEX_ENV') : 'prod';
 if($app['environment'] === 'dev'){
     \Dotenv::load(__DIR__ . '/../');
 }
-$app['debug'] = getenv('SILEX_DEBUG') ? filter_var(getenv('SILEX_DEBUG'), FILTER_VALIDATE_BOOLEAN) : true;
+$app['debug'] = getenv('SILEX_DEBUG') ? filter_var(getenv('SILEX_DEBUG'), FILTER_VALIDATE_BOOLEAN) : false;
 
 $app['cache.path']           = __DIR__ . '/../cache';
 $app['http_cache.cache_dir'] = $app['cache.path'] . '/http';
